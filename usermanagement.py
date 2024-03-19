0967318b6c51710fc53868528d278243e9ed7c23
Paul Wecker <pwecker@air-von-paul-2.speedport.ip>
2024-03-19 09:28:27 +0100

    Added DDD and restructured code.

--- usermanagement.py
+++ usermanagement.py
@@ -1,67 +1,99 @@
-from datetime import date
-from pathlib import Path
-from typing import Optional
-
+from datetime import datetime
 import pandas as pd
+import pickle
+from typing import List
+import os
 
-class UserManager():
-    def __init__(self) -> "UserManager":
-        if Path("budgets").is_file():
-            self.budgets = pd.read_parquet("budgets")
-        else:
-            data = {"account": [],
-                    "budget": []}
-            self.budgets = pd.DataFrame(data=data)
-            self.budgets.to_parquet("budgets")
+print("Welcome to spendings_app.")
 
-        if Path("expenses").is_file():
-            self.expenses = pd.read_parquet("expenses")
-        else:
-            data = {"date": [],
-                    "account": [],
-                    "expense": []}
-            self.expenses = pd.DataFrame(data=data)
-            self.expenses.to_parquet("expenses")
+#Load usernames
+if os.path.exists("users.pkl"):
+    with open("users.pkl", "rb") as f:
+        users_set = pickle.load(f)
+        print("Users loaded.")
+else:
+    print("Creating users-file.")
+    users_set = set()
 
-    @staticmethod
-    def ask_for_account() -> str:
-        print("Please type in your account name.")
-        print("If you don't have an account, a new account will be created.")
-        account = input("Your Account name: ")
-        return account
-    
-    def retrieve_budget(self, account: str) -> Optional[float]:
-        if account not in self.budgets["account"].values:
-            return None
-        else:
-            return self.budgets[self.budgets["account"]==account]["budget"].values
+#Load budgets
+if os.path.exists("budgets.pkl"):
+    with open("budgets.pkl", "rb") as f:
+        budgets = pickle.load(f)
+        print("Budgets loaded")
+else:
+    print("Creating budgets file.")
+    budgets = dict()
 
-    def set_budget(self, account: str) -> float:
-        budget = float(input("Please enter your budget: "))
-        # Use dict instead
-        df = pd.DataFrame(data={"account": [account],
-                                "budget": [budget]})
-        self.budgets = pd.concat([self.budgets, df])
-        return budget
+#Load spendings
+if os.path.exists("spendings.pkl"):
+    with open("spendings.pkl", "rb") as f:
+        spendings = pickle.load(f)
+        print("Spendings loaded.")
+else:
+    print("Creating spendings file in working memory.")
+    spendings = dict()
 
-    def retrieve_expense(self, account) -> float:
-        expense = float(input("How much did you spend? "))
-        d = date.today()
-        df = pd.DataFrame(data={"account": [account],
-                                "date": [d],
-                                "expense": [expense]})
 
-        self.expenses = pd.concat([self.expenses, df])
-        return expense
 
-    def retrieve_remaining_budget(self, account):
-        month = date.today().month
-        budget = self.retrieve_budget(account)
+username = input("Please enter your username: ")
+print("We will check if we can find your account...")
 
-        all_expenses = self.expenses[self.expenses["account"]==account]["expense"].sum()
-        remaining_budget = budget - all_expenses
-        return remaining_budget[0]
-    
-    def save_and_quit(self):
-        self.budgets.to_parquet("budgets")
-        self.expenses.to_parquet("expenses")
+if username in users_set:
+    print("We have retrieved your account from our system.")
+else:
+    print("We could not find your account. Would you like to create a new account with this username?")
+    create_new_account = input("Please respond with y/n: ")
+    if create_new_account == "y":
+        users_set.add(username)
+        with open("users.pkl", "wb") as f:
+            pickle.dump(users_set, f)
+        print(f"Your account has been created with the username <{username}>.")
+    elif create_new_account == "n":
+        print("You chose to not create an account. Thanks for using this app.")
+        quit()
+    else:
+        print("Your response was invalid. The program will stop here but we're happy to see you again.")
+        quit()
+
+# Now retrieve a budget, if existent
+if username in budgets:
+    print(f"Your budget is {budgets[username]}")
+    initiate_new_budget = input("Would you like to set a new budget? (y/n): ")
+    if initiate_new_budget == "y":
+        budgets[username] = int(input("Please enter a new budget as integer: "))
+        print(f"Your new budget is now: {budgets[username]}")
+    else:
+        print("You chose to not change your budget")
+else:
+    print("You haven't set a budget yet.")
+    budgets[username] = int(input("Please enter a new budget as integer: "))
+    print(f"Your new budget is now: {budgets[username]}")
+with open("budgets.pkl", "wb") as f:
+    pickle.dump(budgets, f)
+print("The budgets file has been saved.")
+
+enter_expense = input("Would you like to enter an expense and save it to your spendings table (y/n)? ")
+
+if enter_expense == "y":
+    if username not in spendings:
+        df = pd.DataFrame(columns=['date', 'expense'])
+        df['date'] = pd.to_datetime(df['date'], format='%y-%m-%d')
+
+        spendings[username] = df
+
+    date = datetime.now()
+    print(date)
+    expense_input = float(input("Enter the expense amount: "))
+
+    spendings[username].loc[len(spendings[username])] = [date, expense_input]
+    with open("spendings.pkl", "wb") as f:
+        pickle.dump(spendings, f)
+    print("Your spendings have been recorded. These are your total spendings:")
+    print(spendings[username])
+else:
+    print("You chose to not enter an expense.")
+df = spendings[username]
+df['month'] = df['date'].apply(lambda x: x.month)
+df = df[df["month"]== 3]
+sum = df['expense'].sum()
+print(f"For this month, you have spent X. You still have {budgets[username] - sum} to spend.")
